5a59f386ac67bb3a3ba386d886d1cbfb011d76d6
Deepak Tiwari <tiwarideepak.dk.2004@gmail.com>
2025-11-02 20:35:53 +0530

    some ui change

--- src/components/LeftContent.jsx
+++ src/components/LeftContent.jsx
@@ -3,7 +3,7 @@ const LeftContent = ({changeHandler, addNote, title, text}) => {
         <div className="lg:border-r w-full h-1/2 lg:w-1/2 lg:h-full lg:py-3 px-6">
             <input type="text" value={title} className="w-full p-3 font-bold text-2xl outline-none" placeholder="Enter title..." onChange={changeHandler} />
             <hr className="border-gray-700" />
-            <textarea value={text} className="p-3 w-full resize-none h-1/2 lg:h-[70vh] outline-none text-justify" placeholder="Enter text..." onChange={changeHandler}></textarea>
+            <textarea value={text} className="p-3 w-full resize-none h-[25vh] lg:h-[70vh] outline-none text-justify" placeholder="Enter text..." onChange={changeHandler}></textarea>
             <span className="flex justify-end text-sm">Length: {text.length}</span>
             <button className="w-full bg-white text-gray-900 transition ease-in-out hover:bg-purple-500 hover:text-white active:bg-purple-600 mt-3 py-2 rounded-md" onClick={addNote}>Add</button>
         </div>
